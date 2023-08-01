64749b04074d49e1ba8b924d9ce3efc31d33c14e
BenSandeen <12025856+BenSandeen@users.noreply.github.com>
2023-08-01 12:10:58 -0500

    Testing dropping back to netstandard2.0 and restsharp 108 (#21)
    * Bump RestSharp from 106.5.4 to 106.12.0 in /DripDotNet
    
    Bumps [RestSharp](https://github.com/restsharp/RestSharp) from 106.5.4 to 106.12.0.
    - [Release notes](https://github.com/restsharp/RestSharp/releases)
    - [Changelog](https://github.com/restsharp/RestSharp/blob/dev/releasenotes.md)
    - [Commits](https://github.com/restsharp/RestSharp/commits/106.12)
    
    ---
    updated-dependencies:
    - dependency-name: RestSharp
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Add import for updating RestSharp and clean up whitespace
    
    * Try updating RestSharp to 108.0
    
    * Upgrade `RestSharp` to 108.0.0
    
    * Use NewtonsoftJson for serialization
    
    * Fix DripClient
    
    * Revert to `netstandard2.0` and tests to `netcoreapp3.1`
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Jeremy Haberman <jeremy.haberman@drip.com>

--- DripDotNet/DripClient.cs
+++ DripDotNet/DripClient.cs
@@ -146,20 +146,28 @@ namespace Drip
             options.UserAgent = "Drip DotNet v#" + typeof(DripClient).Assembly.GetName().Version.ToString();
             options.BaseUrl = new System.Uri(BaseUrl);
 
-            JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings
+            // TODO: Fix this once we unblock the customer
+            var addHeaderThing = false;
+
+            if (string.IsNullOrEmpty(AccessToken))
+                options.Authenticator = new HttpBasicAuthenticator(ApiKey, string.Empty);
+            else
+                addHeaderThing = true;
+
+            JsonSerializerSettings defaultSettings = new JsonSerializerSettings
             {
                 ContractResolver = new LcaseUnderscoreMappingResolver(),
                 NullValueHandling = NullValueHandling.Ignore
             };
 
-            var client = new RestClient(options);
+            var client = new RestClient(
+                options,
+                configureSerialization: s => s.UseNewtonsoftJson(defaultSettings)
+            );
             client.AddDefaultHeader("Content-Type", "application/vnd.api+json");
             client.AddDefaultUrlSegment("accountId", AccountId);
-            client.UseNewtonsoftJson(jsonSerializerSettings);
 
-            if (string.IsNullOrEmpty(AccessToken))
-                client.Authenticator = new HttpBasicAuthenticator(ApiKey, string.Empty);
-            else
+            if (addHeaderThing)
                 client.AddDefaultHeader("Authorization", "Bearer #" + AccessToken);
 
             return client;
