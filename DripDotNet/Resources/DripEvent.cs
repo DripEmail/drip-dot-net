3de081469a32fbfc9627687944d547372d91728f
BenSandeen <12025856+BenSandeen@users.noreply.github.com>
2023-08-01 17:41:26 -0500

    Update README and clean up whitespace

--- DripDotNet/Resources/DripEvent.cs
+++ DripDotNet/Resources/DripEvent.cs
@@ -1,18 +1,18 @@
 ﻿/*
  The MIT License (MIT)
- 
+
  Copyright (c) 2015 - 2017 Avenue 81 Inc. d/b/a Leadpages, All Rights Reserved
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -44,8 +44,8 @@ namespace Drip
 
         /// <summary>
         /// Optional. A Boolean specifiying whether we should attach a lead score
-        /// to the subscriber (when lead scoring is enabled). Defaults to false. 
-        /// This flag only applies to new subscribers — if the subscriber already 
+        /// to the subscriber (when lead scoring is enabled). Defaults to false.
+        /// This flag only applies to new subscribers — if the subscriber already
         /// exists, we will silently ignore this.
         /// </summary>
         public bool? PotentialLead { get; set; }
